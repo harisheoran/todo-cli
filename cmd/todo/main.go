a54f83c997e439308bdbda621c2ee1f20edb9695
harisheoran <harisheoran01@gmail.com>
2024-07-05 13:41:32 +0530

    delete, verbose feature

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -2,37 +2,81 @@ package main
 
 import (
 	"cmd/todo"
+	"flag"
 	"fmt"
+	"log"
 	"os"
-	"strings"
 )
 
 func main() {
-	const filename = "task.json"
+	var filename = "task.json"
+	listFlag := flag.Bool("list", false, "List all the tasks")
+	addFlag := flag.Bool("add", false, "Add a task")
+	completeFlag := flag.Int("complete", -1, "Mark the task completed")
+	deleteFlag := flag.Int("delete", -1, "Delete the task")
+	verboseFlag := flag.Bool("details", false, "Detailed View of tasks")
+	flag.Parse()
+
+	if os.Getenv("TODO_FILENAME") != "" {
+		filename = os.Getenv("TODO_FILENAME")
+	}
 
 	list := &todo.List{}
 
 	err := list.Get(filename)
-
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
-	switch len(os.Args) {
-	case 1:
-		for i, value := range *list {
-			fmt.Println("%d . %s", i, value.Task)
+	switch {
+	case *listFlag:
+
+		if len(*list) == 0 {
+			fmt.Println("No tasks")
 		}
-	default:
-		task := strings.Join(os.Args[1:], " ")
+
+		// list all the tasks
+		string := list.PrettyOutput()
+		fmt.Println(string)
+	case *addFlag:
+		task, errGet := list.GetTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			log.Fatal(errGet)
+			os.Exit(1)
+		}
 		list.Add(task)
 		err := list.Save(filename)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			log.Fatal(err)
 		}
+		fmt.Println("Task saved successfully.")
 
+	case *completeFlag != -1 && *completeFlag >= 0:
+		err := list.Complete(*completeFlag)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Task marked completed")
+		errSave := list.Save(filename)
+		if errSave != nil {
+			log.Fatal(err)
+		}
+	case *deleteFlag > -1:
+		err := list.Delete(*deleteFlag)
+		if err != nil {
+			log.Fatal(err)
+		}
+		errSave := list.Save(filename)
+		if errSave != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Task Deleted successfully.")
+	case *verboseFlag:
+		verboseData := list.VerboseOutput()
+		fmt.Println(verboseData)
+
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid Options")
+		os.Exit(1)
 	}
-
 }
--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -1,7 +1,83 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+)
 
-func mainTesting(t *testing.T) {
+var (
+	binaryFile = "mytodo"
+	filename   = ".todo.json"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
+
+	if runtime.GOOS == "windows" {
+		binaryFile += ".exe"
+	}
+
+	cmd := exec.Command("touch", filename)
+	lsErr := cmd.Run()
+	if lsErr != nil {
+		fmt.Println("THIS", lsErr)
+		os.Exit(1)
+	}
+
+	command := exec.Command("go", "build", "-o", binaryFile)
+	err := command.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s:%s", binaryFile, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests...")
+
+	result := m.Run()
+
+	fmt.Println("Cleaning up...")
+	os.Remove(binaryFile)
+	os.Remove(filename)
+
+	os.Exit(result)
+}
+
+func TestCLI(t *testing.T) {
+	task := "Go to the GYM"
+	dir, dirErr := os.Getwd()
+	if dirErr != nil {
+		t.Fatal(dirErr)
+	}
+
+	cmdPath := filepath.Join(dir, binaryFile)
+
+	t.Run("Add new task", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-task", task)
+
+		err := cmd.Run()
+
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("List Task", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-list")
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := task + "\n"
+
+		if expected != string(output) {
+			t.Errorf("Expected %q , result is %s", expected, string(output))
+		}
+
+	})
 
 }
